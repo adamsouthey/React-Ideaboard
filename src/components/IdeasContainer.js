c654014eac0c4d3c2b86ffea414f0dc2884efe96
adamsouthey <adamjsouthey@gmail.com>
2018-03-27 17:42:17 +0100

    latest ideas displayed first

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -21,9 +21,29 @@ class IdeasContainer extends Component {
       .catch(error => console.log(error));
   }
 
+  addNewIdea = () => {
+    Axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div>
+        <button className="newIdeaButton"
+          onClick={this.addNewIdea} >
+          New Idea
+        </button>
         {this.state.ideas.map((idea) => {
           return (<Idea idea={idea} key={idea.id} />);
         })}
