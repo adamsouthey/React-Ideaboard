39e5420ad054ec5dd7c56c1a4a6b13a8a032edb8
adamsouthey <adamjsouthey@gmail.com>
2018-03-27 16:34:44 +0100

    react frontend consuming backend data from Rails API

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class IdeasContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideas: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:3001/api/v1/ideas.json')
+      .then(response => {
+        console.log(response);
+        this.setState({ideas: response.data});
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
-      <div>
+      <div className='App-intro'>
         Ideas
       </div>
     );
