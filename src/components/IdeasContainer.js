6b08c45dcd4ec3dbb9b0868b7a157ebe481482f0
adamsouthey <adamjsouthey@gmail.com>
2018-03-28 14:44:16 +0100

    able to add and edit new ideas in frontend and display changes

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -25,47 +25,50 @@ class IdeasContainer extends Component {
       .catch(error => console.log(error));
   }
 
-  addNewIdea = () => {
-    Axios.post(
-      'http://localhost:3001/api/v1/ideas',
-      { idea:
-        {
-          title: '',
-          body: ''
-        }
-      }
-    )
-      .then(response => {
-        console.log(response);
-        const ideas = update(this.state.ideas, {
-          $splice: [[0, 0, response.data]]
-        });
-        //this indicates that we’ve just added a new idea and we want to edit it immediately.
-        this.setState({
-          ideas: ideas,
-          editingIdeaId: response.data.id
-        });
-      })
-      .catch(error => console.log(error));
-  }
 
-  render() {
-    return (
-      <div>
-        <button className="newIdeaButton"
-          onClick={this.addNewIdea} >
-          New Idea
-        </button>
-        {this.state.ideas.map((idea) => {
-          if(this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} />);
-          } else {
-            return (<Idea idea={idea} key={idea.id} />);
+    addNewIdea = () => {
+      Axios.post(
+        'http://localhost:3001/api/v1/ideas',
+        { idea:
+          {
+            title: '',
+            body: ''
           }
-        })}
-      </div>
-    );
-  }
+        }
+      )
+        .then(response => {
+          console.log(response);
+          const ideas = update(this.state.ideas, {
+            $splice: [[0, 0, response.data]]
+          });
+          this.setState({
+            ideas: ideas,
+            editingIdeaId: response.data.id
+          });
+        })
+        .catch(error => console.log(error));
+    }
+
+
+
+
+    render() {
+      return (
+        <div>
+          <button className="newIdeaButton"
+            onClick={this.addNewIdea} >
+            New Idea
+          </button>
+          {this.state.ideas.map((idea) => {
+            if(this.state.editingIdeaId === idea.id) {
+              return(<IdeaForm idea={idea} key={idea.id} />);
+            } else {
+              return (<Idea idea={idea} key={idea.id} />);
+            }
+          })}
+        </div>
+      );
+    }
 }
 
 export default IdeasContainer;
